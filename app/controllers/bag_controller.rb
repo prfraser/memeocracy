35d93c9204b8d51ef94a9637a453500a588617ca
philip fraser <pr.fraser3@gmail.com>
2017-10-19 15:55:30 +1000

    changed cart page

--- app/controllers/bag_controller.rb
+++ app/controllers/bag_controller.rb
@@ -1,7 +1,21 @@
 class BagController < ApplicationController
+	before_action :set_bag, only: [:destroy]
 
 	def index
 		@bags = current_buyer.bags
 	end
 
+	def destroy
+    @bag.destroy
+    respond_to do |format|
+      format.html { redirect_to bag_index_path, notice: 'Item was removed from cart.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_bag
+    @bag = Bag.find(params[:id])
+  end
 end
